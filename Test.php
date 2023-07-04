a5d544a7f544bb5f1ad425801d92fdf2734732ce
mixasa12 <76475774+mixasa12@users.noreply.github.com>
2023-07-04 11:55:37 +0300

    Update Test.php

--- Test.php
+++ Test.php
@@ -19,30 +19,30 @@
 	<div style="width:10vw; float: left; height: 8vw; margin-top:0vw;margin-left: 5vw;">
 		<img src="img/test2.png" style="width:100%;height:100%;">
 	</div>
-	<div style="float: left; width:15vw; margin-left: 10vw; color:white; font-size: 5vw; margin-top: 0;height: 6vw;margin-top:1.5vw; font-family:'Regular'">
+	<div style="float: left; width:15vw; margin-left: 10vw; color:white; font-size: 5vw; margin-top: 0;margin-top:1vw; font-family:'Regular'">
 	Moonrise 
 	</div>
-	<div style="display: inline-block;  margin-left: 18vw; color:white;  font-size: 1.2vw; margin-top: 0;height: 6vw;margin-top:3.5vw;margin-left: 24vw;">
-		Кофты
+	<div style="display: inline-block;  margin-left: 18vw; color:white;  font-size: 1.6vw; margin-top: 0;margin-top:3.5vw;margin-left: 20vw;">
+		<a href="#kofta" style="color:white;  font-size: 1.6vw;text-decoration: none;"> Кофты</a>
 	</div>
-	<div style="display: inline-block; text-align: center; color:white;  font-size: 1.2vw; margin-top: 0; height: 6vw;margin-top:3.5vw;margin-left: 1vw;">
-		Обувь
+	<div style="display: inline-block; text-align: center; color:white;  font-size: 1.6vw; margin-top: 0; margin-top:3.5vw;margin-left: 1vw;">
+		<a href="#obuv" style="color:white;  font-size: 1.6vw; text-decoration: none;">Обувь</a>
 	</div>
-	<div style="display: inline-block;  text-align: center; color:white;  font-size: 1.2vw; margin-top: 0; height: 6vw;margin-top:3.5vw; margin-left: 1vw;">
-		Футболки
+	<div style="display: inline-block;  text-align: center; color:white;  font-size: 1.6vw; margin-top: 0; margin-top:3.5vw; margin-left: 1vw;">
+		<a href="#futbolka" style="color:white;  font-size: 1.6vw;text-decoration: none;">Футболки</a>
 	</div>
-	<div style="display: inline-block;  text-align: center; color:white; font-size: 1.2vw; margin-top: 0; height: 6vw;margin-top:3.5vw; margin-left: 1vw;">
-		Пальто
+	<div style="display: inline-block;  text-align: center; color:white; font-size: 1.6vw; margin-top: 0; margin-top:3.5vw; margin-left: 1vw;">
+		<a href="#palto" style="color:white;  font-size: 1.6vw;text-decoration: none;">Пальто</a>
 	</div>
-		<div style="display: inline-block;  text-align: center; color:white; font-size: 1.2vw; margin-top: 0; height: 6vw;margin-top:3.5vw; margin-left: 1vw;">
-		Рубашки
+		<div style="display: inline-block;  text-align: center; color:white; font-size: 1.6vw; margin-top: 0; height: 4vw;margin-top:3.5vw; margin-left: 1vw;">
+		<a href="#rub" style="color:white;  font-size: 1.6vw;text-decoration: none;">Рубашки</a>
 	</div>
-		<div style="display: inline-block;  text-align: center; color:white; font-size: 1.2vw; margin-top: 0; height: 6vw;margin-top:3.5vw; margin-left: 1vw;">
-		Худи
+		<div style="display: inline-block;  text-align: center; color:white; font-size: 1.6vw; margin-top: 0; height: 4vw;margin-top:3.5vw; margin-left: 1vw;">
+		<a href="#hudi" style="color:white;  font-size: 1.6vw;text-decoration: none;">Худи</a>
 	</div>
 </div>
-<div class="nadpis" style="font-size: 2.5vw;line-height: 1.5;height: 30vw;background-image: url('img/test4.webp');background-size:100% ;color: black;margin-top: 0;padding-top:15vw;font-weight: 700;">
-	Adidas AG — немецкая транснациональная компания по производству<br> спортивной одежды, обуви и аксессуаров. Это крупнейший производитель<br> спортивной одежды в Европе и второй по величине в мире после Nike.<br> Это холдинговая компания Adidas Group, которая включает в себя Runtastic.<br>
+<div class="nadpis" style="font-size: 2.5vw;line-height: 1.5;height: 34vw;background-image: url('img/test4.webp');background-size:100% ;color: black;margin-top: 0;padding-top:16vw;font-weight: 700; padding-left: 10vw;">
+	Adidas AG — немецкая транснациональная компания<br> по производству спортивной одежды, обуви и аксессуаров.<br> Это крупнейший производитель спортивной одежды в Европе<br> и второй по величине в мире после Nike. Это холдинговая<br> компания Adidas Group, которая включает в себя Runtastic.<br>
 </div>
 <!--
 <div style="color: #606160; font-size: 14px; margin-top:81vw;position: absolute;" class="on_">
@@ -50,17 +50,17 @@
 </div>-->
 <div class="marquee-container">
     <p class="marquee">
-        Новая коллекция - Название коллекции
+        Новая коллекция - Название коллекции -
     </p>
 </div>
 <div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 2vw;color: white;" id="obuv">
 	Обувь
 </div>
-<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -69,10 +69,10 @@
 		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -85,11 +85,11 @@
 <div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw; color: white;" id="kofta">
 	Кофты
 </div>
-<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -98,10 +98,10 @@
 		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style="margin-left: 18vw;margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -114,11 +114,11 @@
 <div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw;color: white;"id="futbolka">
 	Футболки
 </div>
-<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -127,10 +127,10 @@
 		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
-	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
 		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
 		<div class="it" style="width:100%;height:40vw;">
-		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+		<div style="margin-left: 18vw;margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
 			$600
 		</div>
 		</div>
@@ -138,6 +138,94 @@
 		<div class="i_class" style="color:white;">Y3</div>
 		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
+	</div>
+</div>
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 2vw;color: white;" id="hudi">
+	Худи
+</div>
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-left: 18vw;margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+</div>
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw; color: white;" id="rub">
+	Рубашки
+</div>
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin:0 auto;margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+</div>
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw;color: white;"id="palto">
+	Пальто
+</div>
+<div class="brand" style="width:82vw; margin:0 auto; margin-top: 3vw;">
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style=" background:white; margin-left: 18vw;margin-top: 38vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+	<div class="item" style="width:40.5vw;margin:0; height: 46.5vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-left: 18vw;margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
 </div>
 <script type="text/javascript">
 	function buy() {
