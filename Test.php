4696f7c05a54579a5a5f94c11f0f1b7b1d05b595
mixasa12 <76475774+mixasa12@users.noreply.github.com>
2023-07-04 10:43:41 +0300

    UpTest.php

--- Test.php
+++ Test.php
@@ -6,11 +6,8 @@
 	<title></title>
 			<link rel="stylesheet" href="css/test.css" type="text/css"/>
 </head>
-<div> 
-	Новая коллекция - как упоительы в России вечера
-</div>
-<body style="background: #080808; margin:0;">
-<div class="image"><img class="img zoom" src="img/col1.jpg" /></div>
+<body style="background: #080808; margin:0;overflow-x: hidden;">
+<!--<div class="image"><img class="img zoom" src="img/col1.jpg" /></div>
 <!--<div class="nadpis">
 	<span style="opacity: 1;">Feathers Limited</span><br>
 	<span style="opacity: 0.8;">Feathers Limited</span><br>
@@ -18,8 +15,25 @@
 	<span style="opacity: 0.4;">Feathers Limited</span><br>
 	<span style="opacity: 0.2;">Feathers Limited</span>	
 </div>-->
-<div class="nadpis">
-	<span style="opacity: 1;">Информация о компании</span><br>
+<div style="width:99.5vw">
+	<div style="width:10vw; float: left; height: 8vw; margin-top:0vw;margin-left: 5vw;">
+		<img src="img/test2.png" style="width:100%;height:100%;">
+	</div>
+	<div style="float: left; width:15vw; margin-left: 10vw; color:white; font-size: 5vw; margin-top: 0;height: 6vw;margin-top:1.5vw; font-family:'Regular'">
+	Moonrise 
+	</div>
+	<div style="display: inline-block; width:10vw; margin-left: 20vw; color:white; font-size: 2.5vw; margin-top: 0;height: 6vw;margin-top:3.5vw;">
+		Кофты
+	</div>
+	<div style="display: inline-block; width:10vw; text-align: center; color:white; font-size: 2.5vw; margin-top: 0; height: 6vw;margin-top:3.5vw;">
+		Обувь
+	</div>
+	<div style="display: inline-block; width:12vw; text-align: center; color:white; font-size: 2.5vw; margin-top: 0; height: 6vw;margin-top:3.5vw; margin-left: 2vw;">
+		Футболки
+	</div>
+</div>
+<div class="nadpis" style="font-size: 2.5vw;line-height: 1.5;height: 30vw;background-image: url('img/test4.webp');background-size:100% ;color: black;margin-top: 0;padding-top:15vw;font-weight: 700;">
+	Adidas AG — немецкая транснациональная компания по производству<br> спортивной одежды, обуви и аксессуаров. Это крупнейший производитель<br> спортивной одежды в Европе и второй по величине в мире после Nike.<br> Это холдинговая компания Adidas Group, которая включает в себя Runtastic.<br>
 </div>
 <!--
 <div style="color: #606160; font-size: 14px; margin-top:81vw;position: absolute;" class="on_">
@@ -27,204 +41,95 @@
 </div>-->
 <div class="marquee-container">
     <p class="marquee">
-        Новая коллекция - как упоительы в России вечера
+        Новая коллекция - Название коллекции
     </p>
 </div>
-<div class="brand">
-	<div class="item">
-		<div style="width:100%;height: 750px;" onclick="buy()" class="click">
-		<div class="it">
-			<div style="width:100%; height: 30px; background:none;">
-				
-			</div>
-			<div style="width: 60px; height:48px;margin-left:400px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<!--<img src="img\images\images\catalog\Orange_Passion\img1.jpg">-->
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 2vw;color: white;" id="obuv">
+	Обувь
+</div>
+<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
 		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
-		<div class="item">
-		<div style="width:100%;height: 750px;" onclick="buy()" class="click">
-		<div class="it">
-						<div style="width:100%; height: 30px; background:none;">
-				
-			</div>
-			<div style="width: 60px; height:48px;margin-left:400px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<!--<img src="img\images\images\catalog\Orange_Passion\img1.jpg">-->
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
 		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
 		</div>
-	</div>
-		<div class="item">
-		<div style="width:100%;height: 750px;" onclick="buy()" class="click">
-		<div class="it">
-			<div style="width:100%; height: 30px; background:none;">
-				
-			</div>
-			<div style="width: 60px; height:48px;margin-left:400px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<!--<img src="img\images\images\catalog\Orange_Passion\img1.jpg">-->
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
-		</div>
-	</div>
-		<div class="item">
-		<div style="width:100%;height: 750px;" onclick="buy()" class="click">
-		<div class="it">
-		<!--<img src="img\images\images\catalog\Orange_Passion\img1.jpg">-->
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
 </div>
-<!--
-<div class="marquee-container">
-    <p class="marquee2">
-        Exclusive New 2022 Exclusive New 2022
-    </p>
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw; color: white;" id="kofta">
+	Кофты
 </div>
-<!--
-<div class="brand2">
-	<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		
+<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
 		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
-		<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<img src="img\images\images\catalog\Orange_Passion\img1.jpg">
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
 		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
 		</div>
-	</div>
-		<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<img src="img\images\images\catalog\Orange_Passion\img1.jpg">
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
-		</div>
-	</div>
-		<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		\<img src="img\images\images\catalog\Orange_Passion\img1.jpg">
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
-		</div>
-	</div>
-		<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<img src="img\images\images\catalog\Orange_Passion\img1.jpg">
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
-		</div>
-	</div>
-		<div class="item2">
-		<div onclick="buy()" class="click2">
-		<div class="it2">
-			<div style="width:100%; height: 15px; background:none;">
-				
-			</div>
-			<div style="width:40px; height:33px;margin-left:160px; background:white; border-radius: 45px; color:black; text-align: center; padding-top: 7px; font-size: 12px;">
-				Pre-<br>
-				order
-			</div>
-		<img src="img\images\images\catalog\Orange_Passion\img1.jpg">
-		</div>
-		<div class="i_name">Orange Passion</div>
-		<div class="i_class">Hand made</div>
-		<div class="i_price">$860</div>
-		<div class="i_oldprice">$990</div>
-		<div  class="i_stock">Out of stock</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
 		</div>
 	</div>
 </div>
--->
+<div style="font-family: 'Bradley Hand', cursive; font-size: 6vw; text-align:center; width:99.5vw; margin-top: 3vw;color: white;"id="futbolka">
+	Футболки
+</div>
+<div class="brand" style="width:81vw; margin-left:10vw; margin-top: 3vw;">
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+	</div>
+	<div class="item" style="width:40vw;margin:0; height: 50vw; border: 2px solid white; margin-top: 1vw;">
+		<div style="width:100%;height: 50vw;" onclick="buy()" class="click">
+		<div class="it" style="width:100%;height:40vw;">
+		<div style="margin-top: 38vw; background:white; margin-left:18vw;width: auto; font-size:1.5vw;color: white;background: orange; display: inline-block;">
+			$600
+		</div>
+		</div>
+		<div class="i_name" style="color:white;">Y3</div>
+		<div class="i_class" style="color:white;">Y3</div>
+		<div class="i_price" style="color:white;">Bestseller</div>
+		</div>
+</div>
 <script type="text/javascript">
 	function buy() {
 		location.href='Test1.php';
